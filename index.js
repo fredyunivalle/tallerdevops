3ca178cf650c0771492adfe4c8671fc1f28d4f6e
Fredy Ballesteros <fredy.ballesteros@correounivalle.edu.co>
2022-05-02 10:54:41 -0500

    nueva branch

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatUnivalle APP';
+const botName = 'ChatUnivalle APP 2';
 
 // Run when client connects
 io.on('connection', (socket) => {
